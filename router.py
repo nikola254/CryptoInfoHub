14fed9ca8aebab9ea0e1bea602dd1153bf911e8a
Nikolaus <123357336+nikola254@users.noreply.github.com>
2024-04-07 19:59:53 +0300

    Add files via upload

---
+++ router.py
@@ -0,0 +1,18 @@
+# нужен для того чтобы записывать разные виды запросов
+from fastapi import APIRouter
+
+from src.init import cmc_client
+
+router = APIRouter(
+    prefix="/cryptocurrencies"
+)
+
+
+@router.get("")
+async def get_cryptocurrencies():
+    return await cmc_client.get_listing()
+
+
+@router.get("/{currency_id}")
+async def get_cryptocurrency(currency_id: int):
+    return await cmc_client.get_currency(currency_id)
